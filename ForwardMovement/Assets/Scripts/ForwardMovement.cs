d0e45b3dbcfa47d03346c7f136d87665c3b1ea0b
shariful55 <44187158+shariful55@users.noreply.github.com>
2021-01-21 07:45:23 +0600

    Edition1

---
+++ ForwardMovement/Assets/Scripts/ForwardMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForwardMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+    }
+}
